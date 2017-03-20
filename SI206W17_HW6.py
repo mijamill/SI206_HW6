81ea093edf60f3bb7b34604c27a869c5f9f4a092
Michael Miller <mijamill@umich.edu>
2017-03-19 22:32:16 -0400

    Problems 1-9 completed, beginning #10

--- SI206W17_HW6.py
+++ SI206W17_HW6.py
@@ -105,17 +105,17 @@ student_tups_list = list(student_tups)
 print("\n\n***** Problem 5 *****")
 # Use a list comprehension to create a list of Student instances out of the student_tups list you just created in Problem 2, and save that list in a variable called programmers. You should make sure you pass these tests before continuing, as you'll need this list for problems later on!
 
-
+programmers = [Student(*people) for people in student_tups_list]
 
 ## [PROBLEM 6]
 print("\n\n***** Problem 6 *****")
 
 # A Student's programming_productivity is defined as that student's number of programs written divided by the years they have been at UMich.
 
-# Use the Python map function on the programmers list you just created, in order to create an map instance iterator of numbers representing the productivity of each student. Save the map iterator in a variable called prod_iter.
-
+# Use the Python map function on the programmers list you just created, in order to create an map instance iterator of numbers representing the productivity of each student. Save the map iterator in a variable called prod_iter
+prod_iter = map(lambda x: x.num_programs / x.years_UM, programmers)
 ## Write code to cast that iterator to a list. Save that list in the variable prod_list.
-
+prod_list = list(prod_iter)
 ## You may add a method to the Student class if you wish in order to do this, but you do not need to. (If you do, make sure you do not create any syntax errors that keep code/tests from running!)
 
 
@@ -123,7 +123,7 @@ print("\n\n***** Problem 6 *****")
 ## [PROBLEM 7]
 print("\n\n***** Problem 7 *****")
 # Create a list of tuples wherein each tuple has a student's name and productivity value. Save the list of tuples in a variable called names_and_productivities. To do this, you should use a list comprehension (you may also use the zip function, and you may use any variables you have already created).
-
+names_and_productivities = list(zip(names, prod_list))
 ## But be careful that if you use answers from previous problems, you use the LISTs you generated, so that all your tests can still pass and you avoid confusion!
 
 
@@ -132,17 +132,17 @@ print("\n\n***** Problem 7 *****")
 print("\n\n***** Problem 8 *****")
 # Use the Python filter function to select the subset of programmers who have names with 5 or more characters. (i.e. ["Albert","Dinesh","Euijin"]) Your result should be an filter object that points to Student instances. Save that filter iterator in a variable called long_names.
 
-
+long_names = filter(lambda a: len(a.name) > 4, programmers)
 
 ## Then write code to cast the value of long_names to a list and save it in the variable long_names_list. 
-
+long_names_list = list(long_names)
 
 
 ## [PROBLEM 9]
 print("\n\n***** Problem 9 *****")
 
 # Use a list comprehension to generate a LIST of just the names of those Student instances whose name is longer than their seniority (i.e., ["Albert", "Mai", "Dinesh", "Euijin"]). Assign it to a variable called names_with_not_too_much_seniority.
-
+names_with_not_too_much_seniority = [stud.name for stud in programmers if len(stud.name) > stud.years_UM]
 ## Note that you can use another list you have already created for this problem.
 
 
@@ -152,7 +152,12 @@ print("\n\n***** Problem 9 *****")
 print("\n\n***** Problem 10 *****")
 
 ## Define a function called readfiles, which accepts a list of filenames as input and yields each line in each of the file with that name, assuming those files exist in the same directory as this program.
-
+def readfiles(list_of_names):
+    for nam in list_of_names:
+        fileref = open(nam, 'r')
+        for eachLine in fileref:
+            yield eachLine
+        fileref.close()
 ## Define a generator called len_check which accepts a generator of file lines and returns a generator object of all the lines it's accepted whose length is longer than 40 characters.
 
 ## Finally, write a function called main_filterer that accepts a list of filenames (strings), and returns a generator of all the lines in those files that are longer than 40 characters. The function should invoke the other function and generator, readfiles and len_check.
@@ -173,11 +178,11 @@ print("\n\n***** Problem 10 *****")
 
 
 
-# Uncomment this code to test so you can see easily what results from your code. DO uncomment it. DO NOT delete or change it. (You can add other code above while you work, of course.)
-provided_file_names = ["samplehw6_1.txt","samplehw6_2.txt"]
-for ln in main_filterer(provided_file_names):
-    print(ln.rstrip('\n'), end=" ")
-####
+## Uncomment this code to test so you can see easily what results from your code. DO uncomment it. DO NOT delete or change it. (You can add other code above while you work, of course.)
+# provided_file_names = ["samplehw6_1.txt","samplehw6_2.txt"]
+# for ln in main_filterer(provided_file_names):
+#     print(ln.rstrip('\n'), end=" ")
+#####
 
 
 ##### TESTS BELOW THIS LINE. DO NOT CHANGE ANY CODE BELOW THIS LINE. #####
